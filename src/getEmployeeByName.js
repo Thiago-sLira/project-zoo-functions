c8f11cda096ef179098abd4ce6293463023fc8cb
Thiago Lira <thiago.slira@outlook.com>
2022-09-29 18:54:26 -0300

    Requisito 3 concluído

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const el = employees.find((el) => el.firstName === employeeName || el.lastName === employeeName);
+  if (employeeName === undefined) return {};
+  return el;
 }
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
