d5eff6c45ea7920dae458a57dbc008508e4ffd7f
Thiago Lira <thiago.slira@outlook.com>
2022-09-29 17:17:54 -0300

    Requisito 1 concluído

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui oi
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  // console.log(typeof ids);
+  const id = species.filter((element) => {
+    const value = ids.some((value2) => element.id === value2);
+    return value;
+  });
+  return id;
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
