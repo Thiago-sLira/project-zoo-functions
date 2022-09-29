59177fd1a5a5365043c3051ed8055bd0f2fe167a
Thiago Lira <thiago.slira@outlook.com>
2022-09-29 18:20:01 -0300

    Requisito 2 concluído

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const beast = species.find((element) => element.name === animal);
+  const idade = beast.residents.every((number) => number.age >= age);
+  return idade;
 }
+console.log(getAnimalsOlderThan('lions', 10));
 
 module.exports = getAnimalsOlderThan;
