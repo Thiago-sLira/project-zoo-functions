15507068250cecf1060dedee03dc853ef510c659
Thiago Lira <thiago.slira@outlook.com>
2022-09-30 17:39:19 -0300

    Requisito 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (!animal) {
+    species.forEach((nm) => {
+      obj[nm.name] = nm.residents.length;
+    });
+    return obj;
+  } if (!animal.sex) {
+    const filter = species.find((el) => el.name === animal.specie).residents.length;
+    return filter;
+  }
+  const find = species.find((el) => el.name === animal.specie);
+  const filter = find.residents.filter((element) => element.sex === animal.sex).length;
+  return filter;
 }
 
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
