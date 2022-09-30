6e161fbac2b606c76ea5bd5a11aa1278b95c67ff
Thiago Lira <thiago.slira@outlook.com>
2022-09-29 21:52:34 -0300

    Requisito 4 concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,12 +2,23 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+
 function isManager(id) {
-  // seu código aqui
+  const verify = managers.some((el) => el === id);
+  return verify;
 }
-
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const arroz = employees.filter((el) => el.managers.some((elem) => elem === managerId));
+  const names = arroz.map((element) => `${element.firstName} ${element.lastName}`);
+  return names;
 }
-
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0993'));
 module.exports = { isManager, getRelatedEmployees };
