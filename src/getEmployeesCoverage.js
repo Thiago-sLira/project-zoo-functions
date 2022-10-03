1fe538942bddf189c24bfdc70a4bdb6754affc76
Thiago Lira <thiago.slira@outlook.com>
2022-10-03 20:27:17 -0300

    Iniciando requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,5 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+// {
+// id: "4b40a139-d4dc-4f09-822d-ec25e819a5ad", // id da pessoa
+// fullName: "Sharonda Spry", // nome completo: firstName + lastName
+// species: [ "otters", "frogs" ], // espécies as quais a pessoa é responsável
+// locations: [ "SE", "SW" ], // Um array contendo todas as localizações das espécies
+// }
+
 function getEmployeesCoverage() {
   // seu código aqui
 }
