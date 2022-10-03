d97fe912e42bd3b72a3bea0b7258c18c75e87394
Thiago Lira <thiago.slira@outlook.com>
2022-10-03 20:04:17 -0300

    Trabalhando no requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+const weekDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+// const allAnimals = species.filter((objAnimals) => objAnimals === objAnimals.name);
+// console.log(allAnimals);
+
+const animalSchedule = (scheduleTarget) => {
+  const getAnimal = species.find((element) => element.name === scheduleTarget);
+  return getAnimal.availability;
+};
+
+const getDays = (day) => {
+  const filter = species.filter((animalObject) => animalObject.availability.includes(day))
+    .map(({ name }) => name);
+  return filter;
+};
+
+const emptyParam = () => {
+  const obj = {};
+  weekDays.forEach((day) => {
+    if (day === 'Monday') {
+      obj[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      obj[day] = {
+        officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+        exhibition: getDays(day),
+      };
+    }
+  });
+  return obj;
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (weekDays.includes(scheduleTarget)) return { [scheduleTarget]: emptyParam()[scheduleTarget] };
+  if (!scheduleTarget) return emptyParam();
+  return animalSchedule(scheduleTarget);
 }
 
+// console.log(emptyParam().Monday);
+console.log(getSchedule('Monday'));
 module.exports = getSchedule;
