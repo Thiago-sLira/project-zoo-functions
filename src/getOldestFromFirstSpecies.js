fbc0fac160f296b20e18b041691c4412065f070a
Thiago Lira <thiago.slira@outlook.com>
2022-10-03 16:13:10 -0300

    Requisito 10 concluído

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((element) => element.id === id);
+  const firstSpecie = employee.responsibleFor[0];
+  const findAnimal = species.find((element) => element.id === firstSpecie);
+  const findOldest = findAnimal.residents.map((element) => element.age);
+  const maxNumber = Math.max.apply(null, findOldest);
+  const returnOldest = findAnimal.residents.find((element) => element.age === maxNumber);
+  return Object.values(returnOldest);
 }
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
